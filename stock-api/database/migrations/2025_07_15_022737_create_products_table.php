e501999a200ab525fd4b0b89691cb590ec83c8fe
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-15 00:11:26 -0300

    feat: :sparkles: create columns Product table

--- stock-api/database/migrations/2025_07_15_022737_create_products_table.php
+++ stock-api/database/migrations/2025_07_15_022737_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->decimal('price');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
