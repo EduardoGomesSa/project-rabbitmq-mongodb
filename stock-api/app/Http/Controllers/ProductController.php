c09ae355a65eed57aa521a0583152024631aeb40
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-18 00:25:22 -0300

    feat: :sparkles: index method created

--- stock-api/app/Http/Controllers/ProductController.php
+++ stock-api/app/Http/Controllers/ProductController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private Product $product;
+
+    public function __construct() {
+        $this->product = new Product();
+    }
+
+    public function index() {
+        return $this->product->all();
+    }
 }
--- stock-api/routes/api.php
+++ stock-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/hello', function () {
-    return response(['message' => 'Olá mundo!']);
-});
+Route::get('/products', [ProductController::class, 'index']);
