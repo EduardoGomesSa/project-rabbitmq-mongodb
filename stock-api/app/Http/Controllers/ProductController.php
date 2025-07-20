67350a8099f6640018602a1f9609bd0af724ded8
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-20 00:01:18 -0300

    feat: :sparkles: show method created

--- stock-api/app/Http/Controllers/ProductController.php
+++ stock-api/app/Http/Controllers/ProductController.php
@@ -11,27 +11,35 @@ class ProductController extends Controller
 {
     private Product $product;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->product = new Product();
     }
 
-    public function index() {
+    public function index()
+    {
         $resource = ProductResource::collection($this->product->all());
 
         return $resource->response();
     }
 
-    public function store(ProductRequest $request) {
+    public function store(ProductRequest $request)
+    {
         $productExit = $this->product->where('name', $request->name)->first();
 
-        if($productExit) return response(['error'=>'produto ja existe!'], 401);
+        if ($productExit) return response(['error' => 'produto ja existe!'], 401);
 
         $productSaved = $this->product->create($request->all());
 
-        if(!$productSaved) return response(['error'=>'erro ao cadastrar produto'], 401);
+        if (!$productSaved) return response(['error' => 'erro ao cadastrar produto'], 401);
 
         $resource = new ProductResource($productSaved);
 
         return $resource->response()->setStatusCode(201);
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
     }
 }
--- stock-api/routes/api.php
+++ stock-api/routes/api.php
@@ -9,4 +9,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
