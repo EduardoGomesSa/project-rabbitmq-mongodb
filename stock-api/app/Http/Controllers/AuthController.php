aff13488de437bde3c0342e1f4978980200a98a3
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-20 16:43:38 -0300

    feat: :sparkles: register method created

---
+++ stock-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    private User $user;
+
+    public function __construct() {
+        $this->user = new User();
+    }
+
+    public function register(RegisterRequest $request) {
+        $user = $this->user->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+        $user->token = $token;
+
+        $resource = new UserResource($user);
+        return $resource->response()->setStatusCode(201);
+    }
+}
